cfdfbad4f5656817b9629561c39fb2084e530945
Bobby Galli <bobbyg603@gmail.com>
2022-12-22 19:28:55 -0500

    fix: find dump_syms relative to __dirname

--- src/dump-syms.ts
+++ src/dump-syms.ts
@@ -1,17 +1,18 @@
 import util from 'util';
+import path from 'path';
 import * as child_process from 'child_process';
 const exec = util.promisify(child_process.exec);
 
-export async function dumpSyms(path: string): Promise<{ stdout: string, stderr: string}> {
+export async function dumpSyms(filePath: string): Promise<{ stdout: string, stderr: string}> {
     const { platform } = process;
     const isLinux = platform === 'linux';
-
+    
     if (!isLinux) {
         throw new Error(`Platform: ${platform} is not supported`);
     }
 
-    const dumpSymsPath = `./bin/${platform}/dump_syms`;
-    const command = `${dumpSymsPath} ${path}`;
+    const dumpSymsPath = path.join(__dirname, `../bin/${platform}/dump_syms`);
+    const command = `${dumpSymsPath} ${filePath}`;
 
     return exec(command);
 }
