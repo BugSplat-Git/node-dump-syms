64f5c90c8967ec33bd8242917f0c8bd776c430f6
Bobby Galli <bobbyg603@gmail.com>
2023-01-01 15:18:58 -0500

    chore: remove allowed extensions

--- bin/index.ts
+++ bin/index.ts
@@ -1,21 +1,9 @@
 #! /usr/bin/env node
 import { dumpSyms } from "../src/dump-syms";
-import path from 'path';
 import fs from 'fs';
-
-const allowedFileExtensions = [
-    '',
-    '.so',
-    '.node'
-];
 
 const { argv } = process;
 const inputFilePath = argv.at(-1)!;
-const inputFileExtension = path.extname(inputFilePath);
-
-if (!allowedFileExtensions.some(ext => inputFileExtension === ext)) {
-    logErrorAndExit(`Unrecognized extension: ${inputFileExtension}`);
-}
 
 if (!fs.existsSync(inputFilePath)) {
     logErrorAndExit(`File doesn't exist at path: ${inputFilePath}`);
