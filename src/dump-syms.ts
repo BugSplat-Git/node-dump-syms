c949b97617e05d7e0dbd26d06aff3cc1d82caf52
Bobby Galli <bobbyg603@gmail.com>
2023-01-01 14:27:04 -0500

    refactor: use existsSync to check for supported platform

--- src/dump-syms.ts
+++ src/dump-syms.ts
@@ -1,13 +1,13 @@
 import util from 'util';
 import path from 'path';
-import fs from 'fs/promises'
+import fs from 'fs';
 import * as child_process from 'child_process';
 const exec = util.promisify(child_process.exec);
 
 export async function dumpSyms(filePath: string): Promise<{ stdout: string, stderr: string}> {
     const { platform } = process;
     const dumpSymsPath = path.join(__dirname, `../bin/${platform}/dump_syms`);
-    const supported = await exists(dumpSymsPath);
+    const supported = fs.existsSync(dumpSymsPath);
   
     if (!supported) {
         throw new Error(`Platform: ${platform} is not supported`);
@@ -18,11 +18,3 @@ export async function dumpSyms(filePath: string): Promise<{ stdout: string, stde
     return exec(command);
 }
 
-async function exists(path: string): Promise<boolean> {
-    try {
-        await fs.stat(path);
-    } catch {
-        return false;
-    }
-    return true;
-}
