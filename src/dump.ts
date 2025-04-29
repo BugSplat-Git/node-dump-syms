ad9d6951b677eb875d61d5694ff8220d5c5f1033
Bobby Galli <bobbyg603@pm.me>
2025-04-29 09:01:55 -0400

    fix: make inputPath and outputPath required args

--- src/dump.ts
+++ src/dump.ts
@@ -2,5 +2,13 @@ const nodeDumpSyms = require('../native/index.node');
 
 export function dumpSyms(inputPath: string, outputPath: string): void {
     // TODO BG support other options here
+    if (!inputPath) {
+        throw new Error('inputPath is required');
+    }
+
+    if (!outputPath) {
+        throw new Error('outputPath is required');
+    }
+
     return nodeDumpSyms.dumpSymbols([inputPath], outputPath);
 }
